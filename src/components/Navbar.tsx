564db09dfeddd20d8316d99a7a430c9178b851a4
Amandeep Kumar <amandeepkumar.flymediatech@gmail.com>
2026-06-04 15:54:53 +0530

    add social media links,Display the contact number

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Menu, X, ChevronDown } from "lucide-react";
 import { motion, AnimatePresence } from "framer-motion";
-
+import { Phone } from "lucide-react";
 
 const staticNavLinks = [
   { name: "Home", href: "/" },
@@ -79,11 +79,24 @@ export default function Navbar() {
                   />
                 )}
               </Link>
-            ))}
+
+
+            )
+            )}
           </nav>
 
           {/* Booking CTA Button */}
           <div className="hidden lg:block">
+
+            <Link
+               href="tel:+17786880333"
+              className="relative inline-flex items-center justify-center px-6 py-3 border border-luxury-gold hover:border-luxury-gold text-sm font-semibold text-gray-900 bg-transparent hover:bg-luxury-gold hover:text-white transition-all duration-300 group overflow-hidden"
+            >
+              <Phone className=" w-4 h-4 text-luxury-gold" />
+  
+              <span className="relative z-10"> +1 778 688 0333</span>
+              <span className="absolute inset-0 bg-gradient-to-r from-[#D0A511]  opacity-0 group-hover:opacity-100 transition-opacity duration-300 -z-0" />
+            </Link>
             <Link
               href="/booking"
               className="relative inline-flex items-center justify-center px-6 py-3 border border-luxury-gold hover:border-luxury-gold text-sm font-semibold text-gray-900 bg-transparent hover:bg-luxury-gold hover:text-white transition-all duration-300 group overflow-hidden"
